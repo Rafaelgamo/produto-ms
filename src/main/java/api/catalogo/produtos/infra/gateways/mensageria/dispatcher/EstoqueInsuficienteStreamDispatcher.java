dfb349761041b7e4254b20213c9be8f5e52a55ed
caio303 <caioalves_diogo@hotmail.com>
2025-02-18 09:22:17 -0300

    adicionar log

--- src/main/java/api/catalogo/produtos/infra/gateways/mensageria/dispatcher/EstoqueInsuficienteStreamDispatcher.java
+++ src/main/java/api/catalogo/produtos/infra/gateways/mensageria/dispatcher/EstoqueInsuficienteStreamDispatcher.java
@@ -1,5 +1,7 @@
 package api.catalogo.produtos.infra.gateways.mensageria.dispatcher;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Component;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EstoqueInsuficienteStreamDispatcher implements EstoqueInsuficienteDispatcher {
 
+    private static final Logger log = LoggerFactory.getLogger(EstoqueInsuficienteStreamDispatcher.class);
     @Value("${queue.topics.estoque-insuficiente.name}")
     private String estoqueInsuficienteTopicName;
 
@@ -18,6 +21,7 @@ public class EstoqueInsuficienteStreamDispatcher implements EstoqueInsuficienteD
 
     @Override
     public void enviar(Long pedidoId) {
+        log.info("Estoque Insuficiente - enviado para '{}':  {}", estoqueInsuficienteTopicName, pedidoId);
         streamBridge.send(estoqueInsuficienteTopicName, pedidoId);
     }
 
